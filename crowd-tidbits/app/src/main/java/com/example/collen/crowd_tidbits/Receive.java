3d36de68e693dd8f6b86db542a72708a03883ea9
CollenMphabantshi <mickeybanchi@gmail.com>
2015-11-25 19:46:36 +0200

    mobile app

--- crowd-tidbits/app/src/main/java/com/example/collen/crowd_tidbits/Receive.java
+++ crowd-tidbits/app/src/main/java/com/example/collen/crowd_tidbits/Receive.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -97,24 +98,26 @@ public class Receive extends Activity implements GoogleMap.OnMyLocationChangeLis
 
         List<ConnectionHandler.Post> listOfPosts = new ArrayList<ConnectionHandler.Post>();
         listOfPosts = connectionhandler.radius(latitude+"",longitude+"",p+"");
-        GoogleMap map2 = null;
+
 
         if(listOfPosts != null) {
-            if (map2 == null) {
-                map2 = ((MapFragment) getFragmentManager().findFragmentById(R.id.rec_map)).getMap();
+            if (map == null) {
+                map = ((MapFragment) getFragmentManager().findFragmentById(R.id.rec_map)).getMap();
             }
             map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
             for (ConnectionHandler.Post post : listOfPosts) {
               //  if(post.getPriority() ==  p) {
                     LatLng lat_logi = new LatLng(post.getLatitude(), post.getLongitude());
-                    map2.addMarker(new MarkerOptions().position(lat_logi).title(post.description  + "\n Priority: " + post.getPriority()));
-                    map2.addMarker(new MarkerOptions().position(lat_logi).snippet("\n Date: " + post.getPostTime()));
-              //  }
+                    map.addMarker(new MarkerOptions().position(lat_logi).title(post.description  + "\n Priority: " + post.getPriority()));
+                    map.addMarker(new MarkerOptions().position(lat_logi).snippet("\n Date: " + post.getPostTime()));
+                    CameraPosition cameraPosition = new CameraPosition.Builder().target(lat_logi).zoom(14.0f).build();
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                    map.moveCamera(cameraUpdate);
+                //  }
             }
 
-            map2 = null;
-            listOfPosts = null;
+
         }
         else
         {
